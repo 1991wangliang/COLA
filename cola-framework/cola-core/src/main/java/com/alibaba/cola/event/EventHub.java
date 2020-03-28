7d10198de548d0894aee78949f2187e14449aff7
gitee-wangliang <1991wangliang@gmail.com>
2020-03-28 19:19:10 +0800

    responseRepository 没有赋值

--- cola-framework/cola-core/src/main/java/com/alibaba/cola/event/EventHub.java
+++ cola-framework/cola-core/src/main/java/com/alibaba/cola/event/EventHub.java
@@ -1,12 +1,14 @@
 package com.alibaba.cola.event;
 
+import com.alibaba.cola.dto.Response;
 import com.alibaba.cola.exception.framework.ColaException;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ListMultimap;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,12 +21,12 @@ import java.util.Map;
 @SuppressWarnings("rawtypes")
 @Component
 public class EventHub {
+
     @Getter
-    @Setter
     private ListMultimap<Class, EventHandlerI> eventRepository = ArrayListMultimap.create();
-    
+
     @Getter
-    private Map<Class, Class> responseRepository = new HashMap<>();
+    private Map<EventHandlerI, Class<? extends Response>> responseRepository = new HashMap<>();
     
     public List<EventHandlerI> getEventHandler(Class eventClass) {
         List<EventHandlerI> eventHandlerIList = findHandler(eventClass);
@@ -41,6 +43,17 @@ public class EventHub {
      */
     public void register(Class<? extends EventI> eventClz, EventHandlerI executor){
         eventRepository.put(eventClz, executor);
+
+        Type[]  types = executor.getClass().getGenericInterfaces();
+        for(Type type:types){
+            Type[] paramTypes =  ((ParameterizedType)(type)).getActualTypeArguments();
+            if(paramTypes!=null&&paramTypes.length==2){
+                Class responseClazz = paramTypes[0].getClass();
+                if(responseClazz.isInstance(Response.class)){
+                    responseRepository.put(executor,responseClazz);
+                }
+            }
+        }
     }
 
     private List<EventHandlerI> findHandler(Class<? extends EventI> eventClass){
